21649c981c5e2a4ad7c8964cd8d5fd71bd03aa86
Ian McDonnell <mcdoim14@gmail.com>
2023-02-19 17:17:46 -0600

    Testing twitch stuff...

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LandingPage>
+    <TwitchPlayer></TwitchPlayer>
+  </LandingPage>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LandingPage from './components/LandingPage.vue'
+import TwitchPlayer from './components/TwitchPlayer.vue';
+
+Vue.use(LoadScript);
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LandingPage,
+    TwitchPlayer
   }
 }
 </script>
@@ -21,6 +26,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
+
 </style>
